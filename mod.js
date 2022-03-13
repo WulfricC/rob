e50b16179b4e1deb7e88f9608fde35338d58f07c
Wulfric K R Clackson <wulfric.clackson@gmail.com>
2022-03-13 13:21:39 +0000

    added module file

---
+++ mod.js
@@ -0,0 +1,7 @@
+export * from 'reader-writer.js';
+export * from 'built-ins.js';
+export * as encodings from 'encodings.js';
+export * from 'esmod.js';
+export * from 'extern-handler.js';
+export * from 'scheme-handler.js';
+export * from 'symbols.js';
